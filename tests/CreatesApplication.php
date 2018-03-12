55e49fb59d019e5d750595ba6d8386ae0e0c6f49
Christopher Pitt <cgpitt@gmail.com>
2018-03-12 06:21:37 +0200

    Add registration tests

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,18 +2,16 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
     public function createApplication()
     {
+        putenv('DB_DEFAULT=testing');
+
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
@@ -21,5 +19,11 @@ trait CreatesApplication
         Hash::driver('bcrypt')->setRounds(4);
 
         return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh', ['--seed' => true]);
     }
 }
