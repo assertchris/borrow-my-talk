029d351a45f24efdaef6de1adaeb20f1a2907891
Wilbur Powery <wilburpowery@gmail.com>
2018-03-22 06:39:08 -0600

    add tests for password reset functionality

--- tests/Browser/ResetPasswordTest.php
+++ tests/Browser/ResetPasswordTest.php
@@ -13,8 +13,41 @@ class ResetPasswordTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser
-                ->visit('/password/reset')
+                ->visit(route('password.request'))
                 ->assertSee('Reset Password');
+        });
+    }
+
+    public function test_it_detects_missing_fields()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser
+                ->visit(route('password.request'))
+                ->click('@send-password-reset')
+                ->assertSee('email field is required');
+        });
+    }
+
+    public function test_it_detects_non_existing_emails()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser
+                ->visit(route('password.request'))
+                ->type('@email', 'janedoe@example.com')
+                ->click('@send-password-reset')
+                ->assertSee("can't find a user with that e-mail address");
+        });
+    }
+
+    public function test_it_can_reset_a_password()
+    {
+        $this->browse(function(Browser $browser) {
+            $browser
+                ->visit(route('password.request'))
+                ->type('@email', 'cgpitt@gmail.com')
+                ->click('@send-password-reset')
+                ->pause(500)
+                ->assertSee("We have e-mailed your password reset link!");
         });
     }
 }
