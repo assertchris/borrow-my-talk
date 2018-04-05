79a5cb5d950ced53c9ce5f33683b63b409569cc1
Wilbur Powery <wilburpowery@gmail.com>
2018-04-04 19:16:04 -0600

    add a schedule to the command to run daily at 23:45

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('borrow:aggregate-page-views')
+                ->dailyAt('23:45');
     }
 
     /**
