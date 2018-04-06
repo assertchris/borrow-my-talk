739c8f30c3765c7ff2abfc377c03e11f3c95729b
Wilbur Powery <wilburpowery@gmail.com>
2018-04-05 19:07:13 -0600

    remove the unused argument to the everyFifteenMinutes method

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('borrow:aggregate-page-views')->everyFifteenMinutes('23:45');
+        $schedule->command('borrow:aggregate-page-views')->everyFifteenMinutes();
     }
 
     protected function commands()
